3c97f0110bd6d4c334e88fe24247e5ee58281b6c
Gavin Barron <gavin.barron@intergen.org.nz>
2018-03-31 17:11:27 -0700

    changing to a web app bot

--- messages/config.js
+++
@@ -1,8 +0,0 @@
-cfg = {
-    WEBSITE_HOSTNAME: process.env.host || '********.ngrok.io',
-    BOTAUTH_SECRET: 'hIB0tS3crets',
-    AZUREAD_APP_ID:  process.env.azAppId ||'guid-from-apps-dev-microsoft-com',
-    AZUREAD_APP_PASSWORD:  process.env.azAppPassword || 'password-from-apps-dev-microsoft-com',
-    AZUREAD_APP_REALM: 'common'
-}
-module.exports = cfg;
